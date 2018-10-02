560952c3ab5051142109f00df89539952cd15d43
Stephen Wolfe <stephenwolfe1@gmail.com>
2018-10-01 18:34:41 -0700

    Updated repo url

--- Formula/kubedecode.rb
+++ Formula/kubedecode.rb
@@ -1,7 +1,7 @@
 class Kubedecode < Formula
   desc "Decode all parts of a kubernetes secret, no more copypasting!"
   homepage "https://github.com/stephenwolfe1/kubedecode-new"
-  url "https://github.com/stephenwolfe1/kubedecode/archive/v1.5.tar.gz"
+  url "https://github.com/stephenwolfe1/kubedecode-new/archive/v1.5.tar.gz"
   sha256 "7d4dfe683c06ee28115dafb3ead51cfb210558ded8f2fd58918626c03ae481a1"
 
   depends_on "jq"
